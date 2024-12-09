ff9fd2b13ee116dbaac0d8a492d9e4865d996835
Roman Kachurin <r.kachurin@gmail.com>
2024-12-09 13:49:34 +0300

    update

--- work_on/dict_practice.py
+++ work_on/dict_practice.py
@@ -1,3 +1,14 @@
-user_input = input().split()
+user_input = str
+#user_input = input('Введите числа через пробел: ').split()
+user_dict ={}
 
-for count_x in user_input:
+while user_input != '0':
+    user_input = input()
+    user_dict[user_input] = int(user_input)
+
+print (user_dict)
+#for count_x in user_input:
+#    user_dict[count_x[0:3:]]= count_x*2
+
+
+print (user_dict)
